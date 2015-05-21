f47c4ed7aeecc95ae2fdb2e4dcce2610fe5b177c
Brandon Mrsny <bmrsny@gmail.com>
2015-05-21 17:15:35 -0600

    stron refactor on rows

--- notes.rb
+++ notes.rb
@@ -1,126 +1,109 @@
-class Replace
+class Pather
+  attr_accessor :map, :column_start, :path_end, :no_row_path
+  attr_reader :markers
 
-  attr_accessor :replace, :replace_count, :array, :find_char
+  FIND_CHAR = "#"
 
-  def initialize(array, find_char)
-    @array         = array
-    @find_char     = find_char
-    @replace       = false
-    @replace_count = 1
+  def create_path
+    read_map
+    set_coordinates
+    generate_path
+    save_output
   end
 
-  def replace
-    @array.map do |value|
-      @replace = this_one?(value)
-      value = "." if @replace == true
-      value
+  def read_map
+    path_map = ""
+    f = File.open(ARGV[0], "r")
+    f.each_line {|line| path_map << line}
+    @map = path_map.split("\n").map(&:chars)
+  end
+
+  def set_coordinates
+    @markers = map.map.with_index do |row, index|
+      set_marker(row, index) if row.include?(find_char)
     end
   end
 
-  def this_one?(value)
-    @replace_count += 1 if value == @find_char
-    if @replace_count%2 == 0 && value != @find_char
-      true
-    else
-      false
+  def set_marker(row, index)
+    row.each_with_index do |char, char_index|
+      return [index, char_index] if char == find_char
     end
   end
 
+  def generate_path
+    markers.length.times do |index|
+      @index = index
+      draw_path(index) unless next_marker == nil
+    end
+  end
+
+  def draw_path(index)
+    case
+    when valid_row_path? then set_row_connection(current_marker, next_marker)
+    when valid_col_path? then set_col_connection(current_marker, next_marker)
+    else                      set_connection(current_marker, next_marker)
+    end
+  end
+
+  def valid_row_path?
+    current_row == next_row
+  end
+
+  def current_marker
+    markers[@index]
+  end
+
+  def current_row
+    current_marker[0]
+  end
+
+  def next_marker
+    markers[@index + 1]
+  end
+
+  def next_row
+    next_marker[0]
+  end
+
+  def set_row_connection(start, finish)
+    until counter == (ending -1)
+      counter += 1
+      map[row][counter] = "*"
+    end
+  end
+
+  def valid_col_path?(coordinates, index)
+    coordinates[1] == markers[index + 1][1]
+  end
+
+  def set_column_connection(start, finish)
+    char_index = markers[index][1]
+    current_row_index = markers[index][0]
+    until current_row_index >= path_end[0]
+      current_row_index += 1
+      map[current_row_index][char_index] = "*"
+    end
+  end
+
+  def set_connection(start, finish)
+    col = start[1]
+    row = finish[0]
+    connection = [row, col]
+
+    set_column_connection(current_marker, connection)
+    set_row_connection(connection, next_marker)
+    change_temp_connectors(connection)
+  end
+
+  def save_output
+    final_map = map.flat_map(&:join).join("\n") + "\n"
+
+    new_file = File.open(ARGV[1], "w")
+    new_file << final_map
+  end
 end
 
-puts Replace.new(["$","$","$","^", "$","$", "^","$","$","^","$","^"], "^").replace
+path = Pather.new
+path.create_path
 
-#first attempt not a great idea
-#!/usr/bin/env ruby
 
-# class Pather
-#   attr_accessor :rows
-#   attr_reader :path_map
-#
-#   def initialize
-#     @path_map = ""
-#     f =  File.open(ARGV[0], "r")
-#     f.each_line {|line| path_map << line}
-#     @find_char              = "#"
-#     @repace                 = false
-#     @starting_row_index     = nil
-#     @ending_row_index       = nil
-#     @starting_char_position = nil
-#     @ending_char_position   = nil
-#     @row_count              = 0
-#     @found_char_count       = 0
-#     @rows = path_map.split("\n")
-#   end
-#
-#   def create_new_path
-#     row_path
-#     column_path
-#     puts rows
-#   end
-#
-#   def row_path
-#     rows.each do |row|
-#       if row.scan(@find_char).size > 1
-#        #ugghghghghg
-#       end
-#     end
-#   end
-#
-#   #def row_path
-#   #rows.each do |row|
-#   #if row.scan(@find_char).size > 1
-#   #chars = row.chars
-#   #chars.map do |char|
-#   #@replace = this_one?(char)
-#   #char = "*" if @replace == true
-#   #end
-#   #else
-#   #end
-#   #end
-#   #end
-#
-#   #def this_one?(char)
-#   #@row_count += 1 if char == @find_char
-#   #if @row_count % 2 == 0 && char != @find_char
-#   #true
-#   #else
-#   #false
-#   #end
-#   #end
-#
-#   def column_path
-#     set_column_positions
-#
-#     current_index = @starting_row_index
-#     until current_index >= @ending_row_index
-#       current_index += 1
-#       rows[current_index][@starting_char_position] = "*"
-#     end
-#   end
-#
-#   def set_column_positions
-#     rows.each_with_index do |row, index|
-#       set_starting_postition(row, index)
-#       set_ending_position(row, index)
-#     end
-#   end
-#
-#   def set_starting_postition(row, index)
-#     if row.include?(@find_char) && @found_char_count == 0
-#       @found_char_count += 1
-#       @starting_row_index = index
-#       @starting_char_position = row.index(@find_char)
-#     end
-#     #need to set last # in row if column has 2 #s
-#   end
-#
-#   def set_ending_position(row, index)
-#     if row.include?(@find_char) && @found_char_count >= 1
-#       @ending_row_index = index
-#       @ending_char_position = row.index(@find_char)
-#     end
-#   end
-# end
-#
-# path = Pather.new
-# path.create_new_path
